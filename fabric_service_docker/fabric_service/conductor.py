a2aa18c83014bf4be202efe894c894071ab96f66
ubuntu <syunk@foxmail.com>
2023-10-19 01:41:24 +0000

    Updated the fabric_service

--- fabric_service_docker/fabric_service/conductor.py
+++
@@ -1,8 +0,0 @@
-import logging
-from rpe_conn import grpc_server
-
-if __name__ == "__main__":
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s %(levelname)s %(name)s: %(message)s')
-    grpc_server.serve()
